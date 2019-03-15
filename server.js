ce09be918c7ebcf5759fbcfdd00718b91d7c1b80
Mohammad Moshiur Rahman <43156585+moshiurrah@users.noreply.github.com>
2019-03-15 15:10:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -36,6 +36,17 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
